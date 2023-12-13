768f86f8659a6f1844a11bf4afc290da3ce62e4e
Rahul Mool <rahulmool3@gmail.com>
2023-12-13 13:15:05 +0530

    ReadMe

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,9 +1,31 @@
 #include "print.h"
 
 void kernel_main() {
+    int i,j,k,n;
+    char str[100];
+    char helloWorld[] = "hello world";
+    char target[50];
+    str[0]='*';
+    str[1]='*';
+    str[2]='*';
+    str[3]='\0';
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    int i,n;
-    for(i=0;i<5;i++)
-    print_str("Welcome to our 64-bit kernel!\n");
+    print_str("Welcome to our 64-bit kernel!");
+    
+    target[0]=0;
+    target[1]='\0';
+
+    for (i = 0; i < 11; i++) {
+        for (j = 0; j <= helloWorld[i]; j++) {
+        	target[i]=j;
+            print_str(target);
+            print_str("\n");
+            for(k=0;k<10000000;k++);
+            //printf("%s\n",target);
+        }
+        
+        target[i+1]=0;
+        target[i+2]='\0';
+    }
 }
